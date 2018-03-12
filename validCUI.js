95ce13888063ca7eec367161a733384f7c465094
Sebastian Pavel <pavel.sebastian.ionut@gmail.com>
2018-03-12 02:14:02 +0200

    Create validCUI.js

---
+++ validCUI.js
@@ -0,0 +1,23 @@
+        let validCui = cui => {
+            let reverseCui = cui.toString().split('').reverse();
+            let control = reverseCui[0];
+            reverseCui.shift();
+            let sum = 0;
+            let keyTest = ['2', '3', '5', '7', '1', '2', '3', '5', '7'];
+            if (reverseCui.length > 10 || reverseCui.length < 4) {
+                toastr.error('Lungime CUI gresit');
+                return false;
+            }
+            for (let i = 0; i < reverseCui.length; i++) {
+                sum += reverseCui[i] * keyTest[i];
+            }
+            sum = sum * 10 % 11;
+            if (sum == 10) {
+                sum = 0;
+            }
+            if (control != sum) {
+                toastr.error('CUI gresit');
+                return false;
+            }
+            return true;
+        }

58b0107333a078bad2a08fdf4aba2d1af9d101f4
Sebastian-Ionut Pavel <pavel.sebastian.ionut@gmail.com>
2022-05-31 21:01:10 +0300

    fix logic

--- validCUI.js
+++ validCUI.js
@@ -1,21 +1,27 @@
-        let validCui = cui => {
-            let reverseCui = cui.toString().split('').reverse();
-            let control = reverseCui[0];
-            reverseCui.shift();
-            let sum = 0;
-            let keyTest = ['2', '3', '5', '7', '1', '2', '3', '5', '7'];
-            if (reverseCui.length > 10 || reverseCui.length < 4) {
-                return false;
-            }
-            for (let i = 0; i < reverseCui.length; i++) {
-                sum += reverseCui[i] * keyTest[i];
-            }
-            sum = sum * 10 % 11;
-            if (sum == 10) {
-                sum = 0;
-            }
-            if (control != sum) {
-                return false;
-            }
-            return true;
-        }
+const validCui = (cui) => {
+  let reverseCui = cui.toString().split("").reverse();
+  const control = reverseCui[0];
+
+  reverseCui.shift();
+
+  if (reverseCui.length > 10 || reverseCui.length < 4) {
+    return false;
+  }
+
+  const keyTest = ["2", "3", "5", "7", "1", "2", "3", "5", "7"];
+  let sum = 0;
+
+  reverseCui.forEach((num, index) => (sum += num * keyTest[index]));
+
+  sum = (sum * 10) % 11;
+
+  if (sum == 10) {
+    sum = 0;
+  }
+
+  if (control != sum) {
+    return false;
+  }
+
+  return true;
+};
